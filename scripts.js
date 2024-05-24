ea96884f12ceb3fdfefbd39ae3f4b8855f5a93f3
Phillip Bogopane <pm.bogopane@gamil.com>
2024-05-24 16:02:19 +0200

    Create html and script files.

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const names = [
+    "Ashwin",
+    "Sibongile",
+    "Jan-Hendrik",
+    "Sifso",
+    "Shailen",
+    "Frikkie",
+  ];
+  
+  const provinces = [
+    "Western Cape",
+    "Gauteng",
+    "Northern Cape",
+    "Eastern Cape",
+    "KwaZulu-Natal",
+    "Free State",
+  ];
+  
